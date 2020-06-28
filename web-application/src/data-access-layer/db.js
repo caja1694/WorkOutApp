98a9d80928ce38f4caf3d8fed56bc036596f5497
caja1694 <caja1694@student.ju.se>
2020-06-28 18:35:35 +0200

    -db.js

--- web-application/src/data-access-layer/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const connection = new mysql.createConnection({
-  host: 'database',
-  user: 'root',
-  password: 'theRootPassword',
-});
-
-module.exports = connection;
